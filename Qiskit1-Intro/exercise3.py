f8043f62cb524457708feff2b670879d138ec6cf
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2025-04-09 01:23:07 +0200

    [UPDATE] update Qiskit lab 1 exercise 3

--- Qiskit1-Intro/exercise3.py
+++ Qiskit1-Intro/exercise3.py
@@ -1,6 +1,64 @@
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from utils import runStateVector
+
 ## Exercice 3 
 
 # 1 - Implement a C-C-C-X
 # 2 - Idem using only 2-qubit gates
-# Note: Knowing that controlled-S gates is .cs(), use what was done in the previous exercice to implement
+# Note: A Toffoli gate can be decomposed into controlled-S gates
+# Knowing that, use controlled-S gates .cs() to implementx.
 # Print the resulting circuit, and check that it indeed works.
+
+def toffoli_gate():
+    """
+    This function implement a Toffoli gate
+    using only CS gates and CX
+    """
+    qc = QuantumCircuit(3)
+    # Hadamard
+    qc.h(2)
+    # CCZ implementation
+    qc.cs(1,2)
+    qc.cx(0,1)
+    qc.cs(1,2)
+    qc.cx(0,1)
+    qc.cs(0,2)
+    # Hadamard
+    qc.h(2)
+    return qc
+
+def bogus_v2():
+    """
+    This function implement a CCCX
+    only with the previous toffoli function
+    """
+    q = QuantumRegister(5, name="q1")
+    c = ClassicalRegister(5)
+    qc = QuantumCircuit(q, c)
+
+    toffoli = toffoli_gate()
+    qc = qc.compose(toffoli, qubits=[0, 1, 4])
+    qc = qc.compose(toffoli, qubits=[2, 4, 3])
+    qc = qc.compose(toffoli, qubits=[0, 1, 4])
+    return qc
+
+def init_state():
+    """
+    This function initialize the qubits to |11110>
+    to test the CCCX (Bogus) function
+    """
+    qc = QuantumCircuit(5)
+    # Initialize first 3 qubits to one
+    for i in range(4):
+        qc.initialize(1, i)
+    return qc
+
+qc = bogus_v2()
+init = init_state()
+qc = init.compose(qc)
+print("\nThe Bogus v2 circuit is:")
+print(qc)
+print("\nThe input statevector is:")
+runStateVector(init)
+print("\nThe output statevector is:")
+runStateVector(qc)
--- Qiskit1-Intro/utils.py
+++ Qiskit1-Intro/utils.py
@@ -1,24 +1,29 @@
 ## Utils functions for Qiskit manipulations ##
 
-from qiskit import *
 from qiskit_aer import AerSimulator, StatevectorSimulator
 import numpy as np
 
-def processOneState(st): # Length = power of 2
-        s = list(st)
-        if len(s) == 2:
-            return {'0' : s[0], '1' : s[1]}
-        else:
-            a0 = processOneState(s[:len(s)//2])
-            a1 = processOneState(s[len(s)//2:])
-            r = {}
-            for k in a0:
-                r['0' + k] = a0[k]
-            for k in a1:
-                r['1' + k] = a1[k]
-            return r
+def processOneState(st): 
+    """
+    Length = power of 2
+    """
+    s = list(st)
+    if len(s) == 2:
+        return {'0' : s[0], '1' : s[1]}
+    else:
+        a0 = processOneState(s[:len(s)//2])
+        a1 = processOneState(s[len(s)//2:])
+        r = {}
+        for k in a0:
+            r['0' + k] = a0[k]
+        for k in a1:
+            r['1' + k] = a1[k]
+        return r
 
-def printOneState(d): # get a dict as per processStates output
+def printOneState(d):
+    """
+    Get a dict as per processStates output
+    """
     for k in d:
         im = d[k].imag
         re = d[k].real
@@ -30,6 +35,11 @@ def printFinalRes(result):
 
 
 def runStateVector(qc):
+    """
+    Compute the output state vector
+    Print its value in a nice format
+    Return it
+    """
     simulator = StatevectorSimulator()
     job = simulator.run(qc.decompose(reps=6), memory=True)
     job_result = job.result()
@@ -38,6 +48,11 @@ def runStateVector(qc):
     return result
 
 def runSample(qc,howmany):
+    """
+    Run the circuit with a simulator
+    Need measure to get a result
+    Print the result of measures and return a dict of it
+    """
     simulator = AerSimulator()
     job = simulator.run(qc.decompose(reps=6), shots=howmany)
     res = dict(job.result().get_counts(qc))
